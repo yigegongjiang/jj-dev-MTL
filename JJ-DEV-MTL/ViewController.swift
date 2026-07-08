555ab86db57a53003f7f7034e19979ed2c73cf55
yangfan_hailv <fan.yang@elestyle.jp>
2026-07-08 18:20:52 +0900

    Initial Commit

---
+++ JJ-DEV-MTL/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JJ-DEV-MTL
+//
+//  Created by YouHan on 2026/07/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
